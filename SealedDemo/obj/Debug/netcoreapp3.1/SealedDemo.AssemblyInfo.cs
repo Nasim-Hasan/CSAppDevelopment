20f735faccb3b8d1ca6a1908020eb45228c72c79
Nasim-Hasan <nasimcse2k5@gmail.com>
2026-05-29 23:55:08 +0600

    Assembly Info files are generated by the build process and contain metadata about the assembly being built. They are typically not meant to be manually edited, as they are automatically updated with each build.

--- SealedDemo/obj/Debug/netcoreapp3.1/SealedDemo.AssemblyInfo.cs
+++ SealedDemo/obj/Debug/netcoreapp3.1/SealedDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SealedDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b19af4221e37a77e0ba56a43fed8efc186b67621")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0cb44ad81d82f9bc7705440e358ea07749cc148")]
 [assembly: System.Reflection.AssemblyProductAttribute("SealedDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SealedDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
